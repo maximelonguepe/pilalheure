1e43b4e78fc60e15f81f9a2f2d7b038c3675da99
ama62nde <amandine.deraedt62@gmail.com>
2022-12-13 14:26:03 +0100

    Ajustement taille des boutons menu

--- menu.py
+++ menu.py
@@ -10,7 +10,7 @@ def menuView(app_window):
     menu_widow.title("Menu")
     menu_widow.geometry("480x320")
     button_width = 30
-    button_height = 8
+    button_height = 5
     background_color = "#d9d9d9"
     btn = Button(menu_widow, text="Changer heure de prise",
                  command=lambda: changeHeurePrise.changeHeureView(menu_widow), width=button_width, height=button_height,
@@ -22,8 +22,9 @@ def menuView(app_window):
     btn3 = Button(menu_widow, text="Modifer les jours de prise",
                   command=lambda: scanner.scannerMedicament(menu_widow, 0), width=button_width, height=button_height,
                   background=background_color)
-    btn3.place(relx=0, rely=0.5)
+    btn3.place(relx=0, rely=0.3)
     btn4 = Button(menu_widow, text="Remplissage", background=background_color, width=button_width, height=button_height,
                   command=lambda: remplissageView(menu_widow))
-    btn4.place(relx=0.5, rely=0.5)
+    btn4.place(relx=0.5, rely=0.3)
+
     menu_widow.mainloop()
