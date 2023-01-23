d19e464b92ed7366652505cf46ccbd1ba47758ce
maximelonguepe <longuepemaxime73@gmail.com>
2023-01-23 22:28:34 +0100

    Fix bug

--- confirmationPrise.py
+++ confirmationPrise.py
@@ -28,7 +28,7 @@ def confirmer_vue_prise(clock):
     confirmer_prise.geometry("480x320")
     text_font = ("Boulder", 15, 'bold')
     bouton_confirm = Button(confirmer_prise, text="Confirmer la prise",
-                            command=lambda: close_and_quit_thread())
+                            command=lambda: close_and_quit_thread(confirmer_prise))
     bouton_confirm.place(relx=0, rely=0)
     thread = Thread(target=thread_bippe())
     thread.start()
