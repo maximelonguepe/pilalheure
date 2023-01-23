0000ca7920dfc660d74a49cc44b53905fb286291
maximelonguepe <longuepemaxime73@gmail.com>
2023-01-23 22:26:48 +0100

    Fix bug

--- confirmationPrise.py
+++ confirmationPrise.py
@@ -10,6 +10,7 @@ quit_window = 0
 def thread_bippe():
     global quit_window
     while not quit_window:
+        global quit_window
         buzz_on()
         time.sleep(1)
         buzz_off()
@@ -28,7 +29,7 @@ def confirmer_vue_prise(clock):
     confirmer_prise.geometry("480x320")
     text_font = ("Boulder", 15, 'bold')
     bouton_confirm = Button(confirmer_prise, text="Confirmer la prise",
-                            command=lambda: confirmer_prise.destroy())
+                            command=lambda: close_and_quit_thread())
     bouton_confirm.place(relx=0, rely=0)
     thread = Thread(target=thread_bippe())
     thread.start()
