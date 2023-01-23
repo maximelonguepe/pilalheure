60b655c8f7dad33b0b85a4d5f1245e9c0baa25ff
maximelonguepe <longuepemaxime73@gmail.com>
2023-01-23 22:01:20 +0100

    fix bug

--- confirmationPrise.py
+++ confirmationPrise.py
@@ -1,10 +1,11 @@
 import time
+from threading import Thread
 from tkinter import Toplevel
 
 from hardware.buzzer import *
 
 
-def confirmer_vue_prise(clock, prise):
+def confirmer_vue_prise(clock):
     confirmer_prise = Toplevel(clock)
     confirmer_prise.title("Confirmer la prise")
     confirmer_prise.geometry("480x320")
@@ -15,4 +16,5 @@ def confirmer_vue_prise(clock, prise):
         time.sleep(1)
         buzz_off()
         time.sleep(1)
+    thread = Thread(target=thread_verifie_heure)
     confirmer_prise.mainloop()
