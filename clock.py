b3d85f2fffc345a915f958d8163e19682229197c
maximelonguepe <longuepemaxime73@gmail.com>
2023-01-23 21:55:27 +0100

    fix bug

--- clock.py
+++ clock.py
@@ -5,7 +5,6 @@ from tkinter import Label, Tk
 import menu
 from confirmationPrise import confirmer_vue_prise
 from database.rappel import get_rappel_hour
-from hardware.buzzer import buzz_on, buzz_off
 from utils.time_util import full_day_to_int
 
 global app_window
@@ -20,11 +19,6 @@ def thread_verifie_heure():
         day = full_day_to_int(jour_reel)
         if heure_rappel == heur_reelle and minutes_rappel == minutes_reelles:
             confirmer_vue_prise(app_window)
-            while True:
-                buzz_on()
-                time.sleep(1)
-                buzz_off()
-                time.sleep(1)
 
         time.sleep(60)
 
@@ -36,7 +30,6 @@ def digital_clock():
 
 
 if __name__ == '__main__':
-    prise = 0
     app_window = Tk()
     app_window.title("Digital Clock")
     app_window.geometry("480x320")
--- confirmationPrise.py
+++ confirmationPrise.py
@@ -1,10 +1,18 @@
+import time
 from tkinter import Toplevel
 
+from hardware.buzzer import *
 
-def confirmer_vue_prise(clock):
+
+def confirmer_vue_prise(clock, prise):
     confirmer_prise = Toplevel(clock)
     confirmer_prise.title("Confirmer la prise")
     confirmer_prise.geometry("480x320")
     text_font = ("Boulder", 15, 'bold')
-
+    prise = False
+    while prise == False:
+        buzz_on()
+        time.sleep(1)
+        buzz_off()
+        time.sleep(1)
     confirmer_prise.mainloop()
