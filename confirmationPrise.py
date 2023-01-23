40c116d8bbeb771c34bfda39d8338a5e54f9b5a5
maximelonguepe <longuepemaxime73@gmail.com>
2023-01-23 21:49:41 +0100

    fix bug

--- confirmationPrise.py
+++ confirmationPrise.py
@@ -1,5 +1,4 @@
-from tkinter import Toplevel, Label, Button
-from clock import prise
+from tkinter import Toplevel
 
 
 def confirmer_vue_prise(clock):
@@ -7,7 +6,5 @@ def confirmer_vue_prise(clock):
     confirmer_prise.title("Confirmer la prise")
     confirmer_prise.geometry("480x320")
     text_font = ("Boulder", 15, 'bold')
-
-    print(prise)
 
     confirmer_prise.mainloop()
