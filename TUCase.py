5e8d68c112833859b32efab71add4c7de975416d
maximelonguepe <longuepemaxime73@gmail.com>
2023-01-29 19:35:36 +0100

    debug

--- TUCase.py
+++ TUCase.py
@@ -6,10 +6,3 @@ while True:
     for i in range(7):
         enclencher_servo_remplissage_haut(i)
         enclencher_servo_prise_haut(i)
-
-    time.sleep(2)
-    for j in range(7):
-        enclencher_servo_remplissage_bas(j)
-        enclencher_servo_prise_bas(j)
-
-    time.sleep(2)
