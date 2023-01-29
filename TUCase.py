e085b005a73a0247ef0750b9d430128c386fe558
maximelonguepe <longuepemaxime73@gmail.com>
2023-01-29 19:40:08 +0100

    debug

--- TUCase.py
+++ TUCase.py
@@ -1,8 +1,7 @@
 from hardware.multiple_servos import enclencher_servo_remplissage_haut, enclencher_servo_remplissage_bas, \
     enclencher_servo_prise_haut, enclencher_servo_prise_bas
 import time
-while True:
 
-    for i in range(7):
-        enclencher_servo_remplissage_haut(i)
-        enclencher_servo_prise_haut(i)
+for i in range(7):
+    enclencher_servo_remplissage_haut(i)
+    enclencher_servo_prise_haut(i)
