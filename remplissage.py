cf5fe6f2ae6dfce4f0e5e4bcc47c8e9a10a02d08
maximelonguepe <longuepemaxime73@gmail.com>
2023-02-01 01:46:38 +0100

    Modification taille boutons

--- remplissage.py
+++ remplissage.py
@@ -47,11 +47,11 @@ def updateList(label, window, btnbas, btnhaut):
         if page >= int(len(liste_medicaments) / 5):
             btnbas.place_forget()
         else:
-            btnbas.place(relx=0.5, rely=0.5)
+            btnbas.place(relx=0.5, rely=0.55)
         if page == 0:
             btnhaut.place_forget()
         else:
-            btnhaut.place(relx=0, rely=0.5)
+            btnhaut.place(relx=0, rely=0.55)
     elif len(liste_medicaments) == 0:
         buffer = "Remplissage terminé"
         label.config(text=buffer)
@@ -72,10 +72,10 @@ def remplissageView(menu_view):
     label_nom_medicaments = Label(remplissage, background="#d9d9d9", font=list_font)
     label_nom_medicaments.place(relx=0, rely=0.2)
     btnhaut = Button(remplissage, text="Précédent", command=lambda: page_precedente(), width=30)
-    btnhaut.place(relx=0, rely=0.5)
+    btnhaut.place(relx=0, rely=0.55)
     btnbas = Button(remplissage, text="Suivant", command=lambda: page_suivante(), width=30)
-    btnbas.place(relx=0.5, rely=0.5)
-    btn_scanner = Button(remplissage, text="Scanner", command=lambda: scannerMedicament(remplissage, 2), width=60)
+    btnbas.place(relx=0.5, rely=0.55)
+    btn_scanner = Button(remplissage, text="Scanner", command=lambda: scannerMedicament(remplissage, 2), width=60,height=3)
     updateList(label_nom_medicaments, remplissage, btnbas, btnhaut)
     btn_scanner.place(relx=0, rely=0.8)
     remplissage.mainloop()
