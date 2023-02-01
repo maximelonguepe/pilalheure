344c07a6a6e5f341447dde4e5dceb1e7b29028a0
maximelonguepe <longuepemaxime73@gmail.com>
2023-02-01 01:34:20 +0100

    Modification taille boutons

--- scanner.py
+++ scanner.py
@@ -72,8 +72,8 @@ def scannerMedicament(menu, parcours):
     text_font = ("Boulder", 15, 'bold')
     border_width = 25
     label_scan = Label(scan, text="Veuillez scanner le médicament", font=text_font, bd=border_width)
-    label_scan.place(rely=0, relx=0.2)
-    entry = Entry(scan, width=50)
+    label_scan.place(rely=0, relx=0.1)
+    entry = Entry(scan, width=60)
     entry.focus_set()
     entry.place(relx=0, rely=0.2)
     btn = Button(scan, text="Ok", width=40, height=5, command=lambda: versJoursPrise(entry, scan, parcours))
